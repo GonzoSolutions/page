502df7fdb24b982a510c4c406126683ea76bc5d5
daniel <daniel.hermansen@proton.me>
2025-05-30 23:23:16 +0200

    added menu

--- src/menu.tsx
+++ src/menu.tsx
@@ -2,9 +2,31 @@
 // menu.tsx
 //
 
+import "./menu.css";
+
+//
+//
+
 export default () => {
+    const sections = [
+        { id: 0, name: "goals" }, 
+        { id: 1, name: "services" }, 
+        { id: 2, name: "projects" }, 
+        { id: 3, name: "articles" }, 
+        { id: 5, name: "tools" }, 
+        { id: 6, name: "products" }, 
+    ]
     return (
-        <div>
+        <div id="menu">
+            <div id="left"></div>
+            <div id="content">
+                <ul>
+                    {sections.map(
+                        (section) => <li> <a href="#"> {section.name} </a> </li>
+                    )}
+                </ul>
+            </div>
+            <div id="right"></div>
         </div>
     )
 };
