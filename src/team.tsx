b12ba4c949cb61a7f9026de14ff69b74c74d3c80
daniel <daniel.hermansen@proton.me>
2025-05-31 02:00:32 +0200

    updated layout

--- src/team.tsx
+++ src/team.tsx
@@ -8,33 +8,31 @@ import "./team.css";
 //
 
 export default () => {
+    const members = [
+        { id: 0, name: { first: "torvald", last: "ask" }, title: "director" },
+        { id: 1, name: { first: "erik", last: "bjørnøy" }, title: "director" }
+    ]
     return (
         <>
             <div id="team">
                 <div id="left"></div>
                 <div id="content">
-                    <div className="member">
-                        <div id="picture">
-                            <img src="/pictures/employees/001.png" />
-                        </div>
-                        <div id="name">
-                            TORVALD ASK
-                        </div>
-                        <div id="title">
-                            DIRECTOR
-                        </div>
-                    </div>
-                    <div className="member">
-                        <div id="picture">
-                            <img src="/pictures/employees/002.png" />
-                        </div>
-                        <div id="name">
-                            ERIK BJØRNØY
-                        </div>
-                        <div id="title">
-                            DIRECTOR
-                        </div>
-                    </div>
+                    {
+                        members.map(
+                            (member) =>
+                                <div className="member">
+                                    <div id="picture">
+                                        <img src={`/pictures/employees/${member.id}.png`} />
+                                    </div>
+                                    <div id="name">
+                                        {member.name.first} {member.name.last}
+                                    </div>
+                                    <div id="title">
+                                        {member.title}
+                                    </div>
+                                </div>
+                        )
+                    }
                 </div>
                 <div id="right"></div>
             </div>
