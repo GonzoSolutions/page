6080f5d0d5ba517d5a03dc5a96d88a0cbe2b16c7
daniel <daniel.hermansen@proton.me>
2025-05-31 04:34:01 +0200

    added links to linkedin

--- src/team.tsx
+++ src/team.tsx
@@ -4,6 +4,9 @@
 
 import * as Material from '@mui/material';
 
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+//import LaunchIcon from '@mui/icons-material/Launch';
+
 //
 //
 
@@ -15,7 +18,8 @@ import "./team.css";
 export default function Team() {
     const members = [
         { id: 0, name: { first: "torvald", last: "ask" }, title: "director" },
-        { id: 1, name: { first: "erik", last: "bjørnøy" }, title: "director" }
+        { id: 1, name: { first: "erik", last: "bjørnøy" }, title: "director" }, 
+        { id: 0, name: { first: "freak", last: "aleek" }, title: "director" }
     ]
     return (
         <>
@@ -26,17 +30,44 @@ export default function Team() {
                         members.map(
                             (member) =>
                                 <Material.Grid size="grow" display="flex" alignItems="center" justifyContent="center">
-                                    <Material.Card elevation={5} sx={{ minWidth: 200, maxWidth: 345, background: 'none' }}>
-                                        <Material.CardMedia component="img" height="256" image={`/pictures/employees/${member.id}.png`} alt="TORVALD ASK" />
-                                        <Material.CardContent>
-                                            <Material.Typography gutterBottom variant="h5" align="center" style={{ textTransform: 'uppercase', color: '#ff2a3a' }}>
-                                                {member.name.first} {member.name.last}
-                                            </Material.Typography>
-                                            <Material.Typography variant="body2" align="center" style={{ textTransform: 'uppercase' }}>
-                                                {member.title}
-                                            </Material.Typography>
-                                        </Material.CardContent>
-                                    </Material.Card>
+                                    <Material.Box component="a" href={`https://linkedin.com/in/${member.name.first}-${member.name.last}`}>
+                                        <Material.Card elevation={5} sx={{ position: 'relative', minWidth: 200, maxWidth: 345, background: '#060606' }}>
+                                            <Material.Box sx={{ position: 'relative' }}>
+                                                <Material.CardMedia component="img" height="256" image={`/pictures/employees/${member.id}.png`} alt={member.name.first} />
+                                                <Material.Box className="overlay"
+                                                    sx={{
+                                                        position: 'absolute',
+                                                        top: 0,
+                                                        left: 0,
+                                                        width: '100%',
+                                                        height: '100%',
+                                                        bgcolor: 'rgba(0, 0, 0, 0.9)',
+                                                        color: '#fff',
+                                                        display: 'flex',
+                                                        alignItems: 'center',
+                                                        justifyContent: 'center',
+                                                        opacity: 0,
+                                                        transition: 'opacity 500ms',
+                                                        '&:hover': {
+                                                            opacity: 1
+                                                        }
+                                                    }}
+                                                >
+                                                    <LinkedInIcon sx={{ fontSize: '64px', margin: '1em', color: '#ff2a3a' }} />
+                                                </Material.Box>
+                                            </Material.Box>
+                                            <Material.CardContent>
+                                                <Material.Typography gutterBottom variant="h5" align="center" style={{ textTransform: 'uppercase', color: '#ff2a3a' }}>
+                                                    {member.name.first} {member.name.last}
+                                                </Material.Typography>
+                                                <Material.Typography variant="body2" align="center" style={{ textTransform: 'uppercase', color: '#f9f9f9' }}>
+                                                    {member.title}
+                                                </Material.Typography>
+                                            </Material.CardContent>
+                                            
+                                        </Material.Card>
+                                    </Material.Box>
+
                                 </Material.Grid>
                         )
                     }
